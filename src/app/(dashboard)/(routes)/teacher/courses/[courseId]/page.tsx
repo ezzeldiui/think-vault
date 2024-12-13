5d620c6ba62c4436b443f042e988a889e6eebc75
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-13 12:08:13 +0400

    add: course creation api v2

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,4 +1,23 @@
-export default function Page({ params }: { params: { courseId: string } }) {
+import { GetServerSideProps } from 'next';
+
+interface PageProps {
+  params: {
+    courseId: string;
+  };
+}
+
+export default function CourseIdPage({ params }: PageProps) {
   const { courseId } = params;
   return <div>Course Id Page {courseId}</div>;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { courseId } = context.params as { courseId: string };
+  return {
+    props: {
+      params: {
+        courseId,
+      },
+    },
+  };
+};
