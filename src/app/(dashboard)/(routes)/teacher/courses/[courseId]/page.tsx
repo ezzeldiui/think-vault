92e9d791866821633f2a43a8f99551e5e99da545
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-13 13:12:30 +0400

    add: course creation api v10

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,6 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default async function CoursePage({
   params,
