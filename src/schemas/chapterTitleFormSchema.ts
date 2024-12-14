813754ce4727736abb9863e30fd156c2888e0915
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-14 17:42:38 +0400

    add: chapter edit page ui v2

--- src/schemas/chapterTitleFormSchema.ts
+++ src/schemas/chapterTitleFormSchema.ts
@@ -4,5 +4,5 @@ export const chapterTitleFormSchema = z.object({
   title: z
     .string()
     .min(1, { message: "Chapter Title is required" })
-    .max(20, { message: "Title must be less than 20 characters" }),
+    .max(25, { message: "Title must be less than 25 characters" }),
 });
