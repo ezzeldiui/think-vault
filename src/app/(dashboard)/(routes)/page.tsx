4ae0d5b6493fc972487591a03c7c213cab66a895
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-14 15:39:26 +0400

    testing: paypal integration

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,19 @@
+"use client"
+
+import PaypalButton from "@/components/paypal-button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const handleSuccess = (details: any) => {
+    console.log(details);
+  };
   return (
     <>
       <UserButton />
+      <PaypalButton
+        amount={"99.99"}
+        onSuccess={handleSuccess}
+      />
     </>
   );
 }
---
+++ src/components/paypal-button.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  FUNDING,
+  PayPalButtons,
+  PayPalScriptProvider,
+} from "@paypal/react-paypal-js";
+
+type PaypalButtonProps = {
+  amount: string;
+  onSuccess: (details: any) => void;
+};
+
+const PaypalButton: React.FC<PaypalButtonProps> = ({ amount, onSuccess }) => {
+  return (
+    <PayPalScriptProvider
+      options={{
+        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "",
+        currency: "USD",
+      }}
+    >
+      <PayPalButtons
+        fundingSource={FUNDING.PAYPAL}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: amount,
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions?.order?.capture().then((details) => {
+            onSuccess(details);
+          });
+        }}
+      />
+    </PayPalScriptProvider>
+  );
+};
+
+export default PaypalButton;
