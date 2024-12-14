e9dcebb969459b9c0f5c604f4aaad52a8aa0bf3e
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-14 16:15:58 +0400

    remove: paypal integration

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,16 +1,10 @@
 "use client";
-
-import PaypalButton from "@/components/paypal-button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  const handleSuccess = (details: any) => {
-    console.log(details);
-  };
   return (
     <>
       <UserButton />
-      <PaypalButton amount={"0.0001"} onSuccess={handleSuccess} />
     </>
   );
 }
