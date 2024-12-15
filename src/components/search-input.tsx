46c3892286b1760892088a9f448943cbef59f764
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-15 19:03:34 +0400

    fix: search-input.tsx

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,35 +1,3 @@
-"use client";
-
-<<<<<<< HEAD
-import { Input } from "@/components/ui/input";
-import { useDebounce } from "@/hooks/useDebounce";
-import { Search } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-import { parseAsString, useQueryState } from "nuqs";
-import { ChangeEvent, useEffect } from "react";
-
-export function SearchInput() {
-  const [value, setValue] = useQueryState(
-    "title",
-    parseAsString
-      .withDefault("")
-      .withOptions({ clearOnDefault: true, throttleMs: 500 }),
-  );
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
-
-  return (
-    <div className="relative">
-      <Search className="absolute left-2 top-1/2 size-4 -translate-y-1/2 transform text-slate-600" />
-
-      <Input
-        onChange={handleChange}
-        value={value || ""}
-        placeholder="Search"
-        className="w-full bg-slate-100 pl-9 focus-visible:ring-slate-200 md:w-[300px]"
-=======
 import qs from "query-string";
 
 import { useEffect } from "react";
@@ -79,7 +47,6 @@ function SearchInputField() {
         value={value}
         className="w-full bg-slate-100 pl-9 focus-visible:ring-slate-200 md:w-[300px]"
         placeholder="Search"
->>>>>>> b8e208e9faa07d511206563e31f999485333d89d
       />
     </div>
   );
