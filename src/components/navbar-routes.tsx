0674a33134a10da747173d77edcbb9ff3a2b09b9
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-15 19:00:46 +0400

    fix: navbar-routes.tsx

--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -16,32 +16,6 @@ export function NavbarRoutes() {
 
   return (
     <>
-<<<<<<< HEAD
-      {
-        isSearchPage && (
-          <div className="hidden md:block">
-            <SearchInput />
-          </div>
-        )
-      }
-    
-      <div className="ml-auto flex gap-x-2">
-        {isTeacherPage || isPlayerPage ? (
-          <Link href="/">
-            <Button size="sm" variant="ghost">
-              <LogOut className="size-4" />
-              Exit
-            </Button>
-          </Link>
-        ) : (
-          <Link href="/teacher/courses">
-            <Button size="sm" variant="ghost">
-              Teacher mode
-            </Button>
-          </Link>
-        )}
-
-=======
       {isSearchPage && (
         <div className="hidden md:block">
           <SearchInput />
@@ -63,9 +37,29 @@ export function NavbarRoutes() {
             </Button>
           </Link>
         )}
+        {isSearchPage && (
+          <div className="hidden md:block">
+            <SearchInput />
+          </div>
+        )}
 
->>>>>>> b8e208e9faa07d511206563e31f999485333d89d
-        <UserButton afterSignOutUrl="/" />
+        <div className="ml-auto flex gap-x-2">
+          {isTeacherPage || isPlayerPage ? (
+            <Link href="/">
+              <Button size="sm" variant="ghost">
+                <LogOut className="size-4" />
+                Exit
+              </Button>
+            </Link>
+          ) : (
+            <Link href="/teacher/courses">
+              <Button size="sm" variant="ghost">
+                Teacher mode
+              </Button>
+            </Link>
+          )}
+          <UserButton afterSignOutUrl="/" />
+        </div>
       </div>
     </>
   );
