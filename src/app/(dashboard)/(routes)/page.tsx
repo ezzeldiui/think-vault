f3cacf40f89752047ac2d8daf9b8831a2419e29d
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-16 20:19:32 +0400

    add dashboard construction page

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 import { UserButton } from "@clerk/nextjs";
+import { Construction } from "lucide-react";
 
 export default function Home() {
-  return (
-    <>
-      <UserButton />
-    </>
-  );
+  const isHaveStripeAccount = false;
+
+  if (!isHaveStripeAccount) {
+    return (
+      <>
+        <div className="flex size-full flex-col items-center justify-center bg-zinc-200">
+          <Construction size={200} className="text-muted-foreground" />
+          <h1 className="text-4xl font-semibold text-muted-foreground">
+            Under Construction
+          </h1>
+          <p className="text-xs font-bold text-muted-foreground">
+            (never going to get built until i can get a stripe account)
+          </p>
+        </div>
+      </>
+    );
+  }
 }
